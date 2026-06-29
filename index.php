2b5ca89b155ff687023b4cffc7febae3c528d2d9
dpdahal <dp48dahal@gmail.com>
2026-06-29 07:08:47 +0545

    project start

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+$uri =$_GET['uri'] ?? 'home';
+$uri =str_replace('.php','',$uri);
+$title=$uri;
+$uri = $uri.'.php';
+
+$pagePath="./pages/".$uri;
+
+require_once 'header.php';
+
+if(file_exists($pagePath) && is_file($pagePath)){
+    include $pagePath;
+}else{
+    include 'pages/404.php';
+}
+
+require_once 'footer.php';
